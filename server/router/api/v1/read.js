25564d09de4d37a1ffd79b8624755b9afa925109
vikas-1729 <53541816+vikas-1729@users.noreply.github.com>
2020-10-31 00:27:10 +0530

    completed data

--- server/router/api/v1/read.js
+++ server/router/api/v1/read.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const readController = require('../../../controller/api/v1/read');
 
-router.get('id/:id', readController.id); //read by review id
+router.get('/id/:id', readController.id); //read by review id
 router.get('/all', readController.all); //read all review
-router.post('/fetch', readController.fetch); //read all review
+router.post('/fetch', readController.fetch); //fetch review for api
 router.get('/status/:value', readController.status); // read review by status i.e approved or rejected
 router.get('/product/:id', readController.product); //read by product id
 router.get('/user/:id', readController.user); //read by user id
@@ -16,6 +16,3 @@ router.all('/',function(req,res){
 });
 module.exports = router;
 
-//review id :5f997ede3e3c061900078700
-//customer id:5f9921eb4e3aea0328d62645
-// product id:5f9921eb4e3aea0328d62646
